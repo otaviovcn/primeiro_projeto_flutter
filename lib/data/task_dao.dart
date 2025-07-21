0361b920d7e06e1ae3c8c27a943b40077e172e1f
otaviovcn <viinident@gmail.com>
2025-07-21 17:28:05 -0300

    :sparkles: feat: Creates method to find all tasks

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,12 +1,40 @@
+import 'package:sqflite/sqflite.dart';
+
+import '../components/task.dart';
+import 'database.dart';
+
 class TaskDao {
   static const String tableSQL =
       'CREATE TABLE $_tableName('
       '$_name TEXT, '
       '$_difficulty INTEGER '
-      '$_image TEXT)';
+      '$_photo TEXT)';
 
-  static const _tableName = 'taskTable';
+  static const _tableName = 'task_table';
   static const _name = 'name';
   static const _difficulty = 'difficulty';
-  static const _image = 'image';
+  static const _photo = 'photo';
+
+  // * Métodos auxiliares *
+  List<Task> toList(List<Map<String, dynamic>> taskMap) {
+    final List<Task> tasks = [];
+    for (Map<String, dynamic> row in taskMap) {
+      final Task task = Task(taskName: row[_name], taskDifficulty: row[_difficulty], photoUrl: row[_photo]);
+      tasks.add(task);
+    }
+    print('retorna lista com todas as tarefas salvas no db \n$tasks');
+    return tasks;
+  }
+
+  // save(Task task) async {}
+  Future<List<Task>> findAll() async {
+    print('Acessando o findAll...');
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tableName);
+    print('Procurando dados no db... \nencontrado!');
+    return toList(result);
+  }
+
+  // Future<List<Task>> find(String taskName) async {}
+  // delete(String taskName) async {}
 }
