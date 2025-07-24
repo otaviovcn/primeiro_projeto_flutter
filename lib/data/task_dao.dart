e3df2856d366760475469b0aa3da044de8713253
otaviovcn <viinident@gmail.com>
2025-07-24 02:12:12 -0300

    :sparkles: feat: Creates a method to save or update a task

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -19,14 +19,43 @@ class TaskDao {
   List<Task> toList(List<Map<String, dynamic>> taskMap) {
     final List<Task> tasks = [];
     for (Map<String, dynamic> row in taskMap) {
-      final Task task = Task(taskName: row[_name], taskDifficulty: row[_difficulty], photoUrl: row[_photo]);
+      final Task task = Task(
+        taskName: row[_name],
+        taskDifficulty: row[_difficulty],
+        photoUrl: row[_photo],
+      );
       tasks.add(task);
     }
     print('retorna lista com todas as tarefas salvas no db \n$tasks');
     return tasks;
   }
 
-  // save(Task task) async {}
+  Map<String, dynamic> toMap(Task task) {
+    final Map<String, dynamic> taskMap = {}; // Map(); Por que não?:
+    taskMap[_name] = task.taskName;
+    taskMap[_difficulty] = task.taskDifficulty;
+    taskMap[_photo] = task.photoUrl;
+    return taskMap;
+  }
+
+  save(Task task) async {
+    print(" Método save ");
+    final Database db = await getDatabase();
+    final  taskMap = toMap(task);
+    var itemList = await find(task.taskName);
+    if (itemList.isEmpty) {
+      print(" A tarefa não existia  ");
+      return await db.insert(_tableName, taskMap);
+    } else {
+      return await db.update(
+        _tableName,
+        taskMap,
+        where: '$_name = ?',
+        whereArgs: [task.taskName],
+      );
+    }
+  }
+
   Future<List<Task>> findAll() async {
     print('Acessando o findAll...');
     final Database db = await getDatabase();
